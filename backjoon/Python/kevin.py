3d54929fb39b32dffade821e39f3d33043e2e1c1
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-15 09:25:49 +0900

    beakjoon 1389
    solution kevin bacon

---
+++ backjoon/Python/kevin.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+
+def bfs(v):
+    queue = deque([v])
+    visited[v] = 1
+    while queue:
+        target = queue.popleft()
+        
+        for i in graph[target]:
+            if not visited[i]:
+                visited[i] = visited[target] + 1
+                queue.append(i)
+
+n, m = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n + 1)]
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+res = []
+for i in range(1, n + 1):
+    visited = [0] * (n + 1)
+    bfs(i)
+    res.append(sum(visited))
+
+print(res.index(min(res)) + 1)
